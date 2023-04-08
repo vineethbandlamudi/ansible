53c72577f072da968a8e196db6e3c2b17ba2bfc8
vineeth <vineethbandlamudi@gmail.com>
2023-04-08 11:37:50 +0530

    Added Terraform Code

---
+++ terraform/ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_spot_instance_request" "sample" {
+  count                   = local.LENGTH
+  ami                     = "ami-0bb6af715826253bf"
+  spot_price              = "0.0035"
+  instance_type           = "t2.micro"
+  wait_for_fulfillment    = true
+  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
+  tags = {
+    Name                  = element(var.COMPONENTS, count.index)
+  }
+}
+
+
+resource "aws_ec2_tag" "name-tag" {
+  count                   = local.LENGTH
+  resource_id             = element(aws_spot_instance_request.sample.*.spot_instance_id, count.index)
+  key                     = "Name"
+  value                   = element(var.COMPONENTS, count.index)
+}
+
+resource "aws_route53_record" "records" {
+  count                   = local.LENGTH
+  name                    = element(var.COMPONENTS, count.index)
+  type                    = "A"
+  zone_id                 = "Z02459993QYCWJEXGK996"
+  ttl                     = "300"
+  records                 = [element(aws_spot_instance_request.sample.*.private_ip, count.index)]
+}
+
+locals {
+  LENGTH                   = length(var.COMPONENTS)
+}
+
+resource "null_resource" "ansible_inventory" {
+  provisioner "local-exec" {
+    command = "terraform-inventory --output=./ansible/inventory.ini"
+  }
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source          = "hashicorp/aws"
+      version         = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region               = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket             = "devopsbyvb9"
+    key                = "roboshop/ansible/terraform.tfstate"
+    region             = "us-east-1"
+    dynamodb_table     = "terraform"
+  }
+}
